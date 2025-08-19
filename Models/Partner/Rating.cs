0ffbf27ba164a8a3b969e0d82fb9afae96b2355a
divyaSakpal-V2 <divya.sakpal@v2solutions.com>
2025-08-19 15:56:29 +0530

    model added part 2

---
+++ Models/Partner/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Partner
+{
+    public class Rating
+    {
+        public float Rate { get; set;}
+        public int Count { get; set;}
+
+    }
+}
