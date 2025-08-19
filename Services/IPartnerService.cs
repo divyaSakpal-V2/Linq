48a1210af15a9f86b8886a8749d79e4f1ebe742f
divyaSakpal-V2 <divya.sakpal@v2solutions.com>
2025-08-19 15:58:21 +0530

    services added part 3

---
+++ Services/IPartnerService.cs
@@ -0,0 +1,16 @@
+﻿using Models.Partner;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IPartnerService
+    {
+        public Task<List<Product>> GetProductsAsync(int limit);
+    }
+
+}
---
+++ Services/Implementation/PartnerService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Models.Partner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Services.Implementation
+{
+    public class PartnerService : IPartnerService
+    {
+        readonly IMemoryCache _memoryCache;
+        readonly ProductsWebApi _productsWebApi;
+        readonly IConfiguration _configuration;
+
+        public PartnerService(ProductsWebApi productsWebApi, IMemoryCache memoryCache, IConfiguration configuration)
+        {
+            _productsWebApi = productsWebApi;
+            _memoryCache = memoryCache;
+            _configuration = configuration;
+        }
+        public async Task<List<Product>> GetProductsAsync(int limit)
+        {
+            List<Product> products = null;
+            int count = 0;
+            try
+            {
+                _memoryCache.TryGetValue<int>("Count", out count);
+                if (limit!=0 && count != 0 && count >= limit)
+                {
+                    _memoryCache.TryGetValue<List<Product>>("Product", out products);
+                    products = products.Take(limit).ToList();
+                }
+                else
+                { /*Using Refit*/
+                    products = await UsingRefitHttpClient(limit);
+                    if (products != null)
+                    {
+                        _memoryCache.Set<List<Product>>("Product", products);
+                        var cacheTime = Convert.ToDouble(_configuration.GetSection("ProductPartnerIntegration:cacheTime").Value);
+                        _memoryCache.Set<int>("Count", products.Count, new MemoryCacheEntryOptions
+                        {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(cacheTime)
+                        });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return products;
+        }
+        /*Refit http client method*/
+        public async Task<List<Product>> UsingRefitHttpClient(int limit)
+        {
+            return await _productsWebApi.GetProductsAsync(limit);
+        }
+
+        /*Traditional way to call httpclient */
+        public async Task<List<Product>> UsingHttpCLientFactory(int limit)
+        {
+            using (var client = new HttpClient())
+            {
+                var clienturl = "";
+                if (!string.IsNullOrEmpty(clienturl) && clienturl.Contains("{limit}") && limit > 0)
+                {
+                    clienturl = clienturl.Replace("{limit}", limit.ToString());
+                }
+                client.BaseAddress = new Uri(clienturl);
+                var response = client.GetAsync("");
+                response.Result.EnsureSuccessStatusCode();
+                var Jsonstring = await response.Result.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<Product>>(Jsonstring, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+        }
+    }
+}
