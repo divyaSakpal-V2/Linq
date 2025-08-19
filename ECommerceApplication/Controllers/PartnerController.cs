38c8c4b2a0cde060f56d1014ce291770f108c775
divyaSakpal-V2 <divya.sakpal@v2solutions.com>
2025-08-19 15:55:40 +0530

    controller added

---
+++ ECommerceApplication/Controllers/PartnerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace ECommerceApplication.Controllers
+{
+    [ApiController]
+    [Route("Products")]
+    public class PartnerController : Controller
+    {
+        readonly IPartnerService _services;
+        readonly ILogger<PartnerController> _logger;
+        public PartnerController(IPartnerService services, ILogger<PartnerController> logger)
+        {
+            _services = services;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] int limit)
+        {
+            try
+            {
+              var result = await _services.GetProductsAsync(limit);
+
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex);
+            }
+        }
+    }
+}
